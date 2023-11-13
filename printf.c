92d303fff978eb8df69f22a8a0df1285a30fcd50
kush-brian <captainrian0@gmail.com>
2023-11-13 13:53:48 +0300

    task zero

--- printf.c
+++ printf.c
@@ -7,51 +7,41 @@
  */
 int _printf(const char *format, ...)
 {
-    int i = 0;
-    va_list list;
-    int lenst;
+	va_list list;
+	int count = 0;
+	char ch;
 
-    if (format == NULL)
-        return (-1); /*returns error*/
-    va_start(list, format);
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            i++;
-        }
-        else if (*(format + 1) == '%')
-        {
-            write(1, "%", 1);
-            i++;
-            format += 2; /* Skip both '%' characters*/
-            continue;
-        }
-        else if (*(format + 1) == 's')
-        {
-            char *str = va_arg(list, char*);
+	if (format == NULL)
+		return (-1);
+	va_start(list, format);
+	while ((ch = *format++))
+	{
+		if (ch != '%')
+		{
+			write(1, &ch, 1);
+			count++;
+		}
+		else if (*format == '%')
+		{
+			write(1, "%", 1);
+			count++;
+			format++;
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(list, char*);
 
-            lenst = strlen(str);
-            write(1, str, lenst);
-            i += lenst;
-            format += 2; /* Skip the 's' character*/
-            continue;
-        }
-        else if (*(format + 1) == 'c')
-        {
-            char c = va_arg(list, int);
+			count += write(1, str, strlen(str));
+			format++;
+		}
+		else if (*format == 'c')
+		{
+			char c = va_arg(list, int);
 
-            write(1, &c, 1);
-            i++;
-            format += 2; /* Skip the 'c' character*/
-            continue;
-        }
-        write(1, format, 1);
-        i++;
-        format++;
-    }
-    va_end(list);
-    return (i);
+			count += write(1, &c, 1);
+			format++;
+		}
+	}
+	va_end(list);
+	return (count);
 }
-
