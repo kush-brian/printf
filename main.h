6daa67dc6b43c3331a6eafa3ac37efe23939690a
kush-brian <captainrian0@gmail.com>
2023-11-15 04:56:31 +0300

    tasks 0 & 1

--- main.h
+++ main.h
@@ -5,7 +5,15 @@
 #include <unistd.h>
 #include <string.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 int _printf(const char *format, ...);
-int _printf2(const char *format, ...);
+void print_int(va_list types, char buffer[], int *buff_ind);
+int write_int(char buffer[], int is_negative, int start, int end);
+void print_buffer(char buffer[], int *buff_ind);
+void print_char(va_list types, char buffer[], int *buff_ind);
+void print_string(va_list types, char buffer[], int *buff_ind);
+void print_percent(va_list types, char buffer[], int *buff_ind);
 
 #endif
